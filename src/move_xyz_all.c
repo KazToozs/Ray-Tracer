e641f8067d7f7fd50ae797b0e9fa0fe7cbf22a31
ugo belfiore <ugo.belfiore@epitech.eu>
2015-05-18 18:07:57 +0200

    multi-shadow for one light done

--- src/move_xyz_all.c
+++ src/move_xyz_all.c
@@ -5,64 +5,51 @@
 ** Login   <belfio_u@epitech.net>
 ** 
 ** Started on  Mon May  4 12:45:16 2015 ugo belfiore
-** Last update Mon May 18 14:40:08 2015 ugo belfiore
+** Last update Mon May 18 18:06:07 2015 ugo belfiore
 */
 
 #include "mini.h"
 
-static void	move_yz(t_st *s, int which, int value, int i)
+static void	move_bis(t_cam *tmp, int which, int value)
 {
-  /* if (which == 2) */
-  /*   { */
-  /*     d->o.view.y_eyes += value; */
-  /*     while (++i < d->o.sph.check) */
-  /* 	d->o.sph.y_sphere[i] += value; */
-  /*     i = -1; */
-  /*     while (++i < d->o.cy.check) */
-  /* 	d->o.cy.y_cyl[i] += value; */
-  /*     i = -1; */
-  /*     while (++i < d->o.co.check) */
-  /* 	d->o.co.y_cone[i] += value; */
-  /*   } */
-  /* if (which == 3) */
-  /*   { */
-  /*     d->o.view.z_eyes += value; */
-  /*     while (++i < d->o.sph.check) */
-  /* 	d->o.sph.z_sphere[i] += value; */
-  /*     i = -1; */
-  /*     while (++i < d->o.cy.check) */
-  /* 	d->o.cy.z_cyl[i] += value; */
-  /*     i = -1; */
-  /*     while (++i < d->o.sph.check) */
-  /* 	d->o.co.z_cone[i] += value; */
-  /*   } */
+  if (which == 5)
+    {
+      while (tmp != NULL)
+	{
+	  tmp->p.y += value;
+	  tmp = tmp->next;
+	}
+    }
+  if (which == 6)
+    {
+      while (tmp != NULL)
+	{
+	  tmp->p.z += value;
+	  tmp = tmp->next;
+	}
+    }
 }
 
 void	move_xyz_all(t_st *s, int which, int value)
 {
   int	i;
+  t_cam	*tmp;
 
   i = -1;
-  /* if (which == 1) */
-  /*   { */
-  /*     d->o.view.x_eyes += value; */
-  /*     while (++i < d->o.sph.check) */
-  /* 	d->o.sph.x_sphere[i] += value; */
-  /*     i = -1; */
-  /*     while (++i < d->o.cy.check) */
-  /* 	d->o.cy.x_cyl[i] += value; */
-  /*     i = -1; */
-  /*     while (++i < d->o.cy.check) */
-  /* 	d->o.co.x_cone[i] += value; */
-  /*   } */
-  move_yz(s, which, value, i);
-  /* if (which == 4) */
-  /*   while (++i < d->o.lum.check) */
-  /*     d->o.lum.x_lum[i] += value; */
-  /* if (which == 5) */
-  /*   while (++i < d->o.lum.check) */
-  /*     d->o.lum.y_lum[i] += value; */
-  /* if (which == 6) */
-  /*   while (++i < d->o.lum.check) */
-  /*     d->o.lum.z_lum[i] += value; */
+  tmp = s->l;
+  if (which == 1)
+    s->c.p.x += value;
+  if (which == 2)
+    s->c.p.y += value;
+  if (which == 3)
+    s->c.p.z += value;
+  if (which == 4)
+    {
+      while (tmp != NULL)
+	{
+	  tmp->p.x += value;
+	  tmp = tmp->next;
+	}
+    }
+  move_bis(tmp, which, value);
 }
--- src/shadow.c
+++ src/shadow.c
@@ -5,7 +5,7 @@
 ** Login   <belfio_u@epitech.net>
 ** 
 ** Started on  Tue May 12 11:16:50 2015 ugo belfiore
-** Last update Mon May 18 14:30:21 2015 ugo belfiore
+** Last update Mon May 18 17:53:52 2015 ugo belfiore
 */
 
 #include "mini.h"
@@ -22,10 +22,29 @@ static void	check_shadow(t_st *s, t_x *x)
 
 static int	shadow_k(t_st *s)
 {
+  t_cone	*tmp_c;
+  t_cyl		*tmp_cy;
+  t_sph		*tmp_s;
+
+  tmp_c = s->co;
+  tmp_cy = s->cy;
+  tmp_s = s->s;
   s->x.k = 10000;
-  check_shadow(s, &s->s->x);
-  check_shadow(s, &s->co->x);
-  check_shadow(s, &s->cy->x);
+  while (tmp_s != NULL)
+    {
+      check_shadow(s, &tmp_s->x);
+      tmp_s = tmp_s->next;
+    }
+  while (tmp_c != NULL)
+    {
+      check_shadow(s, &tmp_c->x);
+      tmp_c = tmp_c->next;
+    }
+  while (tmp_cy != NULL)
+    {
+      check_shadow(s, &tmp_cy->x);
+      tmp_cy = tmp_cy->next;
+    }
   if (s->pl->k < s->x.k && s->pl->k > 0.000001)
     s->x.k = s->pl->k;
   if (s->x.k > 0.000001 && s->x.k < 1 &&  s->x.k != 10000)
@@ -36,11 +55,30 @@ static int	shadow_k(t_st *s)
   return (0);
 }
 
-int	shadow(t_st *s)
+int		shadow(t_st *s)
 {
-  inter_cyl(&s->x.c, s->cy);
-  inter_cone(&s->x.c, s->co);
-  inter_sphere(&s->x.c, s->s);
+  t_cone	*tmp_c;
+  t_cyl		*tmp_cy;
+  t_sph		*tmp_s;
+
+  tmp_c = s->co;
+  tmp_cy = s->cy;
+  tmp_s = s->s;
+  while (tmp_cy != NULL)
+    {
+      inter_cyl(&s->x.c, tmp_cy);
+      tmp_cy = tmp_cy->next;
+    }
+  while (tmp_c != NULL)
+    {
+      inter_cone(&s->x.c, tmp_c); 
+      tmp_c = tmp_c->next;
+    }
+  while (tmp_s != NULL)
+    {
+      inter_sphere(&s->x.c, tmp_s);
+      tmp_s = tmp_s->next;
+    }
   inter_plan(s, &s->x.c);
   if (shadow_k(s) == -1)
     return (-1);
